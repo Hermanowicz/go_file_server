8e4d36b1a79419e171aa3f0e741b1915a18e8f0a
Tomasz Hermanowicz <thermanowicz@outlook.com>
2022-03-13 11:07:37 +0100

    fixed typos

--- main.go
+++ main.go
@@ -5,30 +5,30 @@ package main
 // generating ssl -> https://devcenter.heroku.com/articles/ssl-certificate-self
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-  "flag"
 )
 
 func main() {
 
-  // default lokation which will be served
-  cwd, err := os.Getwd()
-  
-    if err != nil {
-    log.Fatal("Error wille riding cwd, terrminating app.")
-  }
-  
-  // default port 
-  port := flag.String("port", "5000", "Port to use for file server.")
-  dir := flag.String("dir", cwd, "directory to serve. Default current working dir.")
-  
-  // info for user
+	// default location which will be served
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal("Error wille riding cwd, terminating app.")
+	}
+
+	// default port
+	port := flag.String("port", "5000", "Port to use for file server.")
+	dir := flag.String("dir", cwd, "directory to serve. Default current working dir.")
+
+	// info for user
 	fmt.Printf("Starting file server on PORT: %s with DIR: %s", *port, *dir)
 
-  // meaty part of this program
-  handler := http.FileServer(http.Dir(string(*dir)))
-  log.Fatal(http.ListenAndServe(":"+ *port, handler))
+	// meaty part of this program
+	handler := http.FileServer(http.Dir(string(*dir)))
+	log.Fatal(http.ListenAndServe(":"+*port, handler))
 }
